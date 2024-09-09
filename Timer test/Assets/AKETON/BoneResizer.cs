951e543453c35c6f04c9e4d358138096d576d9f3
aketon <python8965@gmail.com>
2024-09-09 23:29:26 +0900

    shading and highlighting

--- Timer test/Assets/AKETON/BoneResizer.cs
+++ Timer test/Assets/AKETON/BoneResizer.cs
@@ -7,6 +7,8 @@ public class BoneResizer : MonoBehaviour
 {
     public Vector3 scale = Vector3.one;
 
+    
+    
     public int depth = 1;
     
     // Start is called before the first frame update
@@ -27,24 +29,35 @@ public class BoneResizer : MonoBehaviour
         
         this.transform.localScale = scale;
         
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+
+            
+            child.localScale = new Vector3(1.0f / child.lossyScale.x, 1.0f / child.lossyScale.y, 1.0f / child.lossyScale.z);
+        }
+        
         void RecursiveSolve(Transform t, int depth)
         {
             if (depth == 0)
             {
+                for (int i = 0; i < t.childCount; i++)
+                {
+                    var child = t.GetChild(i);
+
+                    var newScale = GetCounterScale();
+
+                    child.localScale = newScale;
+                }
+                
                 return;
             }
             
             for (int i = 0; i < t.childCount; i++)
             {
                 var child = t.GetChild(i);
-                
-                RecursiveSolve(child, depth--);
-                
-                var childScale = child.localScale;
-
-                var newScale = GetCounterScale();
-
-                child.localScale = newScale;
+                child.localScale = Vector3.one;
+                RecursiveSolve(child, depth-1);
             }
         }
         
---
+++ Timer test/Assets/AKETON/CalibrationScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalibrationScript : MonoBehaviour
+{
+    BoneResizer lArmboneResizer;
+    BoneResizer rArmboneResizer;
+    
+    BoneResizer lLegboneResizer;
+    BoneResizer rLegboneResizer;
+    
+    BoneResizer SpineResizer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
